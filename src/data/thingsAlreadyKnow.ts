c5c5d7d5b0d35207e350fc0568ba2f982ef02e06
Jairo Vidomines <jvidomines@gmail.com>
2023-02-16 19:52:10 +0100

    Create thingsAlreadyKnow types

---
+++ src/data/thingsAlreadyKnow.ts
@@ -0,0 +1,18 @@
+import { type ThingsStructure } from "./types";
+
+const thingsAlreadyKnow: ThingsStructure = [
+  {
+    id: 0,
+    name: "play football",
+  },
+  {
+    id: 1,
+    name: "eat",
+  },
+  {
+    id: 2,
+    name: "sleep",
+  },
+];
+
+export default thingsAlreadyKnow;
